1a895feb6b51e47a6c518b77541f965ba33a6f6d
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-26 10:29:32 -0700

    put quotes around values in object

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./SearchBar.css";
 
 const sortByOptions = {
-  "Best Match": best_match,
-  "Highest Rated": rating,
-  "Most Reviewed": review_count,
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
 };
 
 class SearchBar extends React.Component {
