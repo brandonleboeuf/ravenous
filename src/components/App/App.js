74b78bbe1fb43bc7df438734e4a9458c646bd57c
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-26 12:13:53 -0700

    updates to structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,19 @@
 import React from "react";
-import logo from "../../logo.svg";
+// import logo from "../../logo.svg";
 import "./App.css";
-// import SearchBar from "/Users/bleb/DEV/school/codeCademy/ravenous/src/components/SearchBar/SearchBar.js";
 import SearchBar from "./../SearchBar/SearchBar";
-import Business from "./../Business/Business.css";
-import BusinessList from "./../BusinessList/BuisnessList.css";
+import BusinessList from "./../BusinessList/BusinessList.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
 }
 
 export default App;
