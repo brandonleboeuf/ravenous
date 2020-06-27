d200f69738aa9c977b06e1a1455d6d9a14c05046
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-27 16:53:12 -0700

    Added visual feedback for the sorting options at the top of the search bar Set the state of sorting options and input elements Simulated a search query with the “Let’s Go” button Reflect on the knowledge you used in this project. As you do so, consider the following question:
    How can we interact with the Yelp API using the “Let’s Go” button?

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,7 +13,7 @@ class SearchBar extends React.Component {
 
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
-    // this.handleSearch = this.handleSearch.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
 
     this.sortByOptions = {
       "Best Match": "best_match",
@@ -39,6 +39,15 @@ class SearchBar extends React.Component {
 
   handleLocationChange(e) {
     this.setState({ location: e.target.value });
+  }
+
+  handleSearch(e) {
+    this.props.searchYelp(
+      this.state.term,
+      this.state.location,
+      this.state.sortBy,
+      e.preventDefault()
+    );
   }
 
   renderSortByOptions() {
@@ -69,7 +78,7 @@ class SearchBar extends React.Component {
           />
           <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
-        <div className="SearchBar-submit">
+        <div onClick={this.handleSearch} className="SearchBar-submit">
           <a>Let's Go</a>
         </div>
       </div>
