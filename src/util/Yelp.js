e58855d29c5770e365830dcb92bd372c947260f9
Brandon LeBoeuf <brandon.leboeuf@gmail.com>
2020-06-28 12:09:48 -0700

    removed hard coding and utalized YELP api

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,12 +2,37 @@
 Client ID
 lDx9D92jXnyDB470f95pAQ
 */
-
 const apiKey =
   "RIbJsHgpMv2_dFvBO1RKNz6W67lGwTfAMTVuxDzTDMMm4wpISJQC0-qPDItxHDA2sGi3IlAjHKWvpbte353lOLDAjvozos1I3dyv-eihBsckbRJKfAB4U1Igz9T4XnYx";
 
-const Yelp = {};
+const Yelp = {
+  searchYelp(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      { headers: { Authorization: `Bearer ${apiKey}` } }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+  },
+};
 
-function search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
-}
+export default Yelp;
